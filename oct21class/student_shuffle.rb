29b0636a41017b1ed25db0819c3340c8509ccde1
Tripp Cook <cook.tripp@gmail.com>
2015-10-21 10:33:26 -0400

    paired up students

--- oct21class/student_shuffle.rb
+++ oct21class/student_shuffle.rb
@@ -1,4 +1,14 @@
 students = %w(Andy Betty Carl Dani Edgar Frank Grant Henrietta Jenny)
+	# %w is a shortcut telling ruby that it is an array
 
-puts students
-puts "#{students}"
+students.shuffle!
+
+count = 0
+
+until count == students.length - 1
+
+	puts "#{students[count]} & #{students[count+1]}"
+	count += 2
+
+end
+
